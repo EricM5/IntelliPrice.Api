266f46ebb05c399b6f73cdd8287ffd2c225105ba
Eric Marzouk <emarzouk@modulim.com>
2022-08-12 15:41:40 -0700

    new dev branch

--- app.py
+++ app.py
@@ -112,18 +112,18 @@ def GetSortedList(soup):
 
 
 def GetSoup(searchterm):
-    # options = ChromeOptions()
-    # options.headless = True
-    # driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe",)
-    # driver.get("https://www.google.com/search?" + "q=" + searchterm + "&tbm=shop")
-    
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    options = ChromeOptions()
+    options.headless = True
+    driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe",)
     driver.get("https://www.google.com/search?" + "q=" + searchterm + "&tbm=shop")
+    
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    # driver.get("https://www.google.com/search?" + "q=" + searchterm + "&tbm=shop")
 
 
 
